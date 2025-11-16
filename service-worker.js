0e08b8ab2c5bf663fb99c1d14037040dfd6abafb
Sharkmaker3000 <matthewthechampion@gmail.com>
2025-11-16 01:15:50 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+const cacheName = 'whiteout-skulls-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './game.js'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
